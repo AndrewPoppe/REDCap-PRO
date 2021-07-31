9d95a12aa29ee2ed940e7db445c2dc76040000dd
Andrew Poppe <AndrewBPoppe@gmail.com>
2021-07-31 18:35:04 -0400

    refine cc log query

--- cc_logs.php
+++ cc_logs.php
@@ -181,7 +181,7 @@ $columns = [
     "token_valid"
 ];
 
-$tableData = $module->queryLogs("SELECT ".implode(', ', $columns));
+$tableData = $module->queryLogs("SELECT ".implode(', ', $columns)." AND (project_id IS NULL OR project_id IS NOT NULL)");
 
 ?>
 
