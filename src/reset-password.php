b099a07c7e7b8559c1ddcdb1ac8d9d3ead0a02a6
Andrew Poppe <AndrewBPoppe@gmail.com>
2021-08-16 15:00:19 -0400

    address security issue

--- src/reset-password.php
+++ src/reset-password.php
@@ -110,7 +110,7 @@ if ($verified_participant) {
         </div>
         <div class="form-group">
             <label>New Password</label>
-            <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+            <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>">
             <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
         </div>
         <div class="form-group">
