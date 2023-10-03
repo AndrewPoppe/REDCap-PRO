6a29acf46b6848255b9666ca1141e0b53eaea7e0
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-10-02 22:35:13 -0400

    bug fix

--- src/register.php
+++ src/register.php
@@ -203,8 +203,7 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
                 title="Register the participant but do not enroll">Register</button>
             <button type="button" class="btn btn-primary"
                 title="Automatically enroll this participant in the study once they are registered"
-                onclick="getDagAndSubmit();">Register and
-                Enroll</button>
+                onclick="getDagAndSubmit();">Register and Enroll</button>
         </div>
         <input type="hidden" name="dag">
         <input type="hidden" name="redcap_csrf_token" value="<?= $module->framework->getCSRFToken() ?>">
@@ -230,6 +229,9 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
                 inputValue: '',
                 showCancelButton: true
             });
+            if (result.isDismissed) {
+                return;
+            }
             selectedDag = result.value;
             $('input[name="dag"]').val(selectedDag);
         }
