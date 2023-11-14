f8a5a67ad1e9068232d4084af44ca0fc72f8d97a
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-11-14 10:57:10 -0500

    Remove test code

--- src/home.php
+++ src/home.php
@@ -39,19 +39,4 @@ $ui->ShowHeader("Home");
 </div>
 
 <?php
-
-try {
-    $auth = new Auth('REDCapPRO');
-    $email = 'test@test.com';
-    $secret = $auth->create_totp_mfa_secret();
-    $otpauth = $auth->create_totp_mfa_otpauth($email, $secret);
-
-    var_dump($otpauth);
-
-    print "<img src='".$auth->get_totp_mfa_qr_url($otpauth, $module)."'>";
-
-} catch (\Throwable $e) {
-    echo "". $e->getMessage() ."";
-}
-
 include_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
